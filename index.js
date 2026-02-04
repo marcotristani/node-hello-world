51bf0c6db6822021cff48c6f4371e33932606853
marcotristani <marco.tristani@hotmail.it>
2026-02-04 12:51:59 +0100

    flat: eseguita piccola logica in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Boolean");
+const numero = "15";
+const numInt = parseInt(numero);
+
+let risposta = "";
+
+if (numInt % 2 === 0) {
+  risposta = "pari";
+} else {
+  risposta = "dispari";
+}
+
+console.log(risposta);
